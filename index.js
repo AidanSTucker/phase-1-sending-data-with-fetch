1b902201eab25258f62abb1273bd921b138127fb
elpatron <aidanseantucker@gmail.com>
2023-06-08 15:57:05 -0400

    pasing tests

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+
+
+    const configureObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch('http://localhost:3000/users', configureObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        document.body.innerHTML = data.id;
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+  }
+
+
+
+
+
